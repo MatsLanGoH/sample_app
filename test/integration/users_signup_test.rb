da99fe86cf5c4ed642fa4b2b94684f294fd78b84
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-08-15 15:40:52 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar"} }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Mini Tester",
+                                         email: "mini@me.com",
+                                         password:              "supertest",
+                                         password_confirmation: "supertest"} }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert.alert-success'
+  end
+
+end
