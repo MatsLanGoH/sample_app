2c1dff5e08f1311ee6cb10fed446a7dff80cc486
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-08-18 23:16:42 +0900

    Implement advanced logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)  # Initial implementation
     if @user.save
       log_in @user
+      remember @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user  # replaces redirect_to user_url(@user)
     else
